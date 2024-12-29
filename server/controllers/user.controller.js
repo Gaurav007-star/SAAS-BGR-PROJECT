03529dc6aa6988e945a44e3e8039fc07f70fe12a
Gaurav007-star <gauravsarkar1000b@gmail.com>
2024-12-29 18:07:07 +0530

    setup

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,22 +1,21 @@
-import {Webhook} from "svix"
+import { Webhook } from "svix";
 
+// https://localhost:4000/api/user/webhooks
+const clerkWebhok = async (req, res) => {
+  try {
+    const whook = new Webhook(process.env.SIGNING_SECRET);
 
-// https://
-const clerkWebhok = async(req,res)=>{
-    try {
-        const whook = new Webhook(process.env.SIGNING_SECRET)
-
-        const responseData = await whook.verify(req.body,{
-            // 
-        })
-
-
-    } catch (error) {
-        console.log(error.message);
-        res.status(400).json({
-            success:false,
-            message:error.message
-        })
-        
-    }
-}
+    const responseData = await whook.verify(req.body, {
+      //
+      "svix-id": req.headers["svix-id"],
+      "svix-signature": req.headers["svix-signature"],
+      "svix-timestamp": req.headers["svix-timestamp"]
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
