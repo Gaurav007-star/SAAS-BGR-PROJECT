282d70713cfd4df5199b808985b98a299a6829f9
Gaurav007-star <gauravsarkar1000b@gmail.com>
2024-12-30 17:35:12 +0530

    dd

--- client/src/store/slices/User.slices.js
+++ client/src/store/slices/User.slices.js
@@ -1,15 +1,22 @@
 /* eslint-disable no-unused-vars */
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { data } from "react-router-dom";
 
-export const UserThunk = createAsyncThunk("UserThunk", async (data) => {
+export const UserThunk = createAsyncThunk("UserThunk", async (userdata) => {
   try {
+    const data = {
+      event: "user.created",
+      userdata
+    };
+
     const response = await axios.post(
       `https://saas-bgr-project.vercel.app/api/user/webhooks`,
       data,
       {
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
+          "Clerk-Signature": import.meta.env.CLERK_SECRET
         }
       }
     );
